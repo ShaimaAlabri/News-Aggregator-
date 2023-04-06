9cbc97a236772071764a071f2810d3583393afb3
unknown <shaima96alabri@hotmail.com>
2023-04-05 22:56:14 -0700

    create Article class

---
+++ src/main/java/org/example/Article.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Article {
+    private String title;
+    private String author;
+    private String date;
+    private String category;
+    private String content;
+}
