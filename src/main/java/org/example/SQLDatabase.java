6f271aaa4a142796111c234d49ddccf45d3a52ff
unknown <shaima96alabri@hotmail.com>
2023-04-05 23:34:16 -0700

    create SQLDatabase class

---
+++ src/main/java/org/example/SQLDatabase.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class SQLDatabase {
+
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=YOUR_DB_NAME_HERE";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "root";
+
+}
